34bfb3aedf4485f528c4b1d8926b04bccb3a2774
Antoine WAAG <antoine.waag@adeliom.com>
2019-10-03 11:27:44 +0200

    test

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _utils = require("src/scripts/_utils");
-
-Object.keys(_utils).forEach(function (key) {
-  if (key === "default" || key === "__esModule") return;
-  Object.defineProperty(exports, key, {
-    enumerable: true,
-    get: function get() {
-      return _utils[key];
-    }
-  });
-});
+require('./src/scripts/_utils');
+require('./src/scripts/_emitter');
