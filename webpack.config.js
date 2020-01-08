e58d7135d3926333bade54e1e74c3dcfe0c2eea1
Antoine WAAG <antoine.waag@adeliom.com>
2020-01-08 09:29:35 +0100

    add webpack with minify

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    mode: "production",
+    entry: "./src/utils",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "utils.js",
+        library: "Dauphine JS",
+        libraryTarget: "umd",
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+                output: {
+                    comments: false,
+                },
+            },
+            extractComments: false
+        })],
+    },
+};
