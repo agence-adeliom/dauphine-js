f2002dd186bd70de5cf18aef4c7e224195d533d0
Antoine WAAG <antoine.waag@adeliom.com>
2020-02-04 10:03:38 +0100

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+const webpack = require("webpack");
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
+const webpack_rules = [];
+
+const webpackOption = {
     mode: "production",
     entry: "./src/utils",
     output: {
@@ -24,3 +27,17 @@ module.exports = {
         })]
     },
 };
+
+let babelLoader = {
+    test: /\.js$/,
+    exclude: /(node_modules|bower_components)/,
+    use: {
+        loader: "babel-loader",
+        options: {
+            presets: ["@babel/preset-env"]
+        }
+    }
+};
+
+webpack_rules.push(babelLoader);
+module.exports = webpackOption;
