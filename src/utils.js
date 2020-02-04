a5c3a64982c85675faeb5b097a030efeb61617c4
Antoine WAAG <antoine.waag@adeliom.com>
2020-02-04 12:32:23 +0100

    test

--- src/utils.js
+++ src/utils.js
@@ -755,6 +755,6 @@ export const animation = (start, end, duration, easing, callback) => {
     }, 1000 / 60);
 };
 
-export default {
-    test: truncate()
-};
+export default function test() {
+    return 'test';
+}
