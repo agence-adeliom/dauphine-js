8177a019b25b937a41c25ec36f745d43b946a3e8
Antoine WAAG <antoine.waag@adeliom.com>
2020-03-04 11:26:40 +0100

    add polyfill for find methods

--- dist/emitter.js
+++ dist/emitter.js
@@ -91,11 +91,12 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
-/* 0 */
+/* 0 */,
+/* 1 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
+import './polyfill.js';
 import {easing} from "../partials/easing";
 
 /*!
